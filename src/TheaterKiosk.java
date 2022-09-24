1c129efd0a54089a535406bbb0f04d392146d326
phetlasn <Souriya.n.phet@gmail.com>
2022-09-23 23:33:51 -0400

    Initial commit

---
+++ src/TheaterKiosk.java
@@ -0,0 +1,11 @@
+public class TheaterKiosk {
+    public static void main(String[] args) {
+        int userAge = 21;
+        System.out.println("Enter your age.");
+
+        if (userAge >= 21){
+            System.out.println("Your age is: " + userAge);
+            System.out.println("You get a wrist band.");
+        }
+    }
+}
